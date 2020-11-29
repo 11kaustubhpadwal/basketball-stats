ad4308d6ead73202346b66d8eeb7737e9fb2af4d
Kaustubh Padwal <11kaustubhpadwal@gmail.com>
2020-11-29 19:07:55 +0100

    FAQ page created.

--- src/components/faq page/FAQ.js
+++ src/components/faq page/FAQ.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { Grid, Text, Collapse } from "@geist-ui/react";
 
 const FAQ = () => {
-  return <div>FAQ</div>;
+  return (
+    <Grid xs={24} md={24} lg={24} style={{ padding: "0 5% 3%" }}>
+      <Grid xs={24} md={24} lg={24}>
+        <Text h3>Frequently asked questions</Text>
+      </Grid>
+      <Grid xs={24} md={24} lg={24}>
+        <Collapse title="Where do the stats come from?" shadow>
+          <Text>
+            The stats come from an open source REST API owned by balldontlie.io
+          </Text>
+        </Collapse>
+        <Collapse title="Is this data legit?" shadow>
+          <Text>
+            The data is legit and is maintained by an open source community of
+            developers.
+          </Text>
+        </Collapse>
+        <Collapse title="How frequently is the data updated?" shadow>
+          <Text>
+            The data is updated every 10 minutes to ensure the most latest and
+            accurate information is provided to the users.
+          </Text>
+        </Collapse>
+        <Collapse title="What is the purpose of this project?" shadow>
+          <Text>
+            The purpose of this project is to provide a condensed version of the
+            NBA stats website to all basketball fans around the world.
+          </Text>
+        </Collapse>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default FAQ;
